603c291acfbad4e6a8380a4c235d53cc415776f4
Alex Makarewycz <alexander_makarewycz@intuit.com>
2022-03-24 01:26:52 +0000

    looking at new york census data

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-c
 
 tabtitle = 'Virginia Counties'
 sourceurl = 'https://www.kaggle.com/muonneutrino/us-census-demographic-data'
-githublink = 'https://github.com/austinlasseter/dash-virginia-counties'
+githublink = 'https://github.com/amakarewycz/dash-virginia-counties'
 varlist=['TotalPop', 'Men', 'Women', 'Hispanic',
        'White', 'Black', 'Native', 'Asian', 'Pacific', 'VotingAgeCitizen',
        'Income', 'IncomeErr', 'IncomePerCap', 'IncomePerCapErr', 'Poverty',
@@ -23,7 +23,18 @@ varlist=['TotalPop', 'Men', 'Women', 'Hispanic',
        'WorkAtHome', 'MeanCommute', 'Employed', 'PrivateWork', 'PublicWork',
        'SelfEmployed', 'FamilyWork', 'Unemployment', 'RUCC_2013']
 
-df=pd.read_pickle('resources/va-stats.pkl')
+varlist=['CountyId', 'State', 'County', 'TotalPop', 'Men', 'Women', 'Hispanic',
+       'White', 'Black', 'Native', 'Asian', 'Pacific', 'VotingAgeCitizen',
+       'Income', 'IncomeErr', 'IncomePerCap', 'IncomePerCapErr', 'Poverty',
+       'ChildPoverty', 'Professional', 'Service', 'Office', 'Construction',
+       'Production', 'Drive', 'Carpool', 'Transit', 'Walk', 'OtherTransp',
+       'WorkAtHome', 'MeanCommute', 'Employed', 'PrivateWork', 'PublicWork',
+       'SelfEmployed', 'FamilyWork', 'Unemployment']
+
+#df=pd.read_pickle('resources/va-stats.pkl')
+df=pd.read_csv('resources/acs2017_county_data.csv')
+state = "New York"
+df=df[df['State']=="New York"]
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -34,29 +45,26 @@ app.title=tabtitle
 ########### Layout
 
 app.layout = html.Div(children=[
-    html.H1('Virginia Census Data 2017'),
-    # Dropdowns
-    html.Div(children=[
-        # left side
-        html.Div([
+    html.H1(f'{state} Census Data 2017'),
+            html.Div(children=[
+            html.Div([
                 html.H6('Select census variable:'),
                 dcc.Dropdown(
                     id='stats-drop',
                     options=[{'label': i, 'value': i} for i in varlist],
                     value='MeanCommute'
                 ),
-        ], className='three columns'),
-        # right side
-        html.Div([
-            dcc.Graph(id='va-map')
-        ], className='nine columns'),
-    ], className='twelve columns'),
-
+                ], className='three columns'),
+                html.Br(),
+                html.A('Code on Github', href=githublink),
+                html.Br(),
+                html.A("Data Source", href=sourceurl),],
+                className='nine columns'),
+    # Dropdownsa
+            html.Div([
+              dcc.Graph(id='va-map')
+            ], className='nine columns')
     # Footer
-    html.Br(),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A("Data Source", href=sourceurl),
     ]
 )
 
@@ -67,7 +75,7 @@ def display_results(selected_value):
     valmin=df[selected_value].min()
     valmax=df[selected_value].max()
     fig = go.Figure(go.Choroplethmapbox(geojson=counties,
-                                    locations=df['FIPS'],
+                                    locations=df['CountyId'],
                                     z=df[selected_value],
                                     colorscale='Blues',
                                     text=df['County'],
@@ -76,7 +84,7 @@ def display_results(selected_value):
                                     marker_line_width=0))
     fig.update_layout(mapbox_style="carto-positron",
                       mapbox_zoom=5.8,
-                      mapbox_center = {"lat": 38.0293, "lon": -79.4428})
+                      mapbox_center = {"lat": 43.29, "lon": -73.93})
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 # https://community.plot.ly/t/what-colorscales-are-available-in-plotly-and-which-are-the-default/2079
